5c61823e50b331ee8871d50ed289f70a58c9e69d
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-18 16:00:08 +0100

    - move nextStep button to the another method; - add allStep button; - add some documentation;

--- src/kasperek/gui/GamePane.java
+++ src/kasperek/gui/GamePane.java
@@ -12,15 +12,33 @@ import java.awt.*;
 
 public class GamePane extends JPanel {
     private JButton nextStep;
+    private JButton allSteps;
+
+    /**
+     * Default pane constructor. It is responsibility for setter all needed parameters for window.
+     */
 
     public GamePane() {
         this.setLayout(null);
         this.setBackground(Color.DARK_GRAY);
+        initializeNextStepButton();
+        initializeAllStepButton();
+    }
 
+
+    private void initializeNextStepButton() {
         nextStep = new JButton("Next step");
-        nextStep.setBounds(990, 10, 100, 40);
+        nextStep.setBounds(995, 10, 100, 40);
 
         this.add(nextStep);
         nextStep.setVisible(true);
+    }
+
+    private void initializeAllStepButton() {
+        allSteps = new JButton("All steps");
+        allSteps.setBounds(1095, 10, 100, 40);
+
+        this.add(allSteps);
+        allSteps.setVisible(true);
     }
 }
