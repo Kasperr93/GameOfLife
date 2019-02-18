1ded61197169695ed36e2d820379d0f2ab0af578
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-18 15:27:54 +0100

    - implementation the main method.

--- src/kasperek/main/Main.java
+++ src/kasperek/main/Main.java
@@ -1,8 +1,19 @@
 package kasperek.main;
 
+import kasperek.gui.GameWindow;
+
 public class Main {
 
+    /**
+     * @author Tomasz Kasperek
+     * @version 1.0 02/18/2019
+     * @see GameWindow
+     * @see kasperek.logic.GameLogic
+     * @since 0.1
+     */
+
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-	// write your code here
+        var window = new GameWindow();
     }
 }
