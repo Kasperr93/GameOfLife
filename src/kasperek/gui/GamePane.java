d383c26b11d8700251b9ad910a832d8b26835dcb
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-18 15:45:00 +0100

    - create basic version of window.

--- src/kasperek/gui/GamePane.java
+++ src/kasperek/gui/GamePane.java
@@ -1,4 +1,26 @@
 package kasperek.gui;
 
-public class GamePane {
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Tomasz Kasperek
+ * @version 0.1 02/18/2019
+ * @see GameWindow
+ * @since 0.1
+ */
+
+public class GamePane extends JPanel {
+    private JButton nextStep;
+
+    public GamePane() {
+        this.setLayout(null);
+        this.setBackground(Color.DARK_GRAY);
+
+        nextStep = new JButton("Next step");
+        nextStep.setBounds(990, 10, 100, 40);
+
+        this.add(nextStep);
+        nextStep.setVisible(true);
+    }
 }
--- src/kasperek/gui/GameWindow.java
+++ src/kasperek/gui/GameWindow.java
@@ -1,4 +1,32 @@
 package kasperek.gui;
 
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Tomasz Kasperek
+ * @version 0.1 02/18/2019
+ * @see GamePane
+ * @since 0.1
+ */
+
 public class GameWindow {
+    private JFrame frame;
+    private GamePane pane;
+
+    /**
+     * Default constructor. It is responsibility for setter all needed parameters for window.
+     */
+
+    public GameWindow() {
+        frame = new JFrame("Game of Life");
+        pane = new GamePane();
+
+        frame.setSize(1200, 800);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setBackground(Color.DARK_GRAY);
+        frame.setContentPane(pane);
+        frame.setVisible(true);
+    }
 }
