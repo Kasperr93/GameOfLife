4fcc8f9542900bf3e7c7adf321816a62aa37e643
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-19 16:27:45 +0100

    - add game logic object; - add constant variable - cell size; - add setter to game logic; - override paint component method;

--- src/kasperek/gui/GamePane.java
+++ src/kasperek/gui/GamePane.java
@@ -1,18 +1,25 @@
 package kasperek.gui;
 
+import kasperek.logic.GameLogic;
+
 import javax.swing.*;
 import java.awt.*;
 
 /**
  * @author Tomasz Kasperek
- * @version 0.1.5 02/18/2019
+ * @version 0.2 02/18/2019
  * @see GameWindow
+ * @see GameLogic
  * @since 0.1
  */
 
 public class GamePane extends JPanel {
+    private GameLogic game;
+
     private JButton nextStep;
     private JButton allSteps;
+
+    private final int CELL_SIZE = 20;
 
     /**
      * Default pane constructor. It is responsibility for setter all needed parameters for window.
@@ -21,6 +28,7 @@ public class GamePane extends JPanel {
     public GamePane() {
         this.setLayout(null);
         this.setBackground(Color.DARK_GRAY);
+
         initializeNextStepButton();
         initializeAllStepButton();
     }
@@ -39,5 +47,31 @@ public class GamePane extends JPanel {
 
         this.add(allSteps);
         allSteps.setVisible(true);
+    }
+
+    void setGameLogic(GameLogic game) {
+        this.game = game;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int x = game.getBoard().length;
+        int y = game.getBoard()[0].length;
+
+        g.setColor(Color.DARK_GRAY);
+
+        for (int i = 0; i <= x; i++) {
+            g.drawLine(i * CELL_SIZE, 0, i * CELL_SIZE, y * CELL_SIZE);
+        }
+
+        g.fillRect(x * CELL_SIZE, 0, 5, y * CELL_SIZE);
+
+        for (int i = 0; i <= y; i++) {
+            g.drawLine(0, i * CELL_SIZE, x * CELL_SIZE, i * CELL_SIZE);
+        }
+
+        game.paintBoard(g);
     }
 }
