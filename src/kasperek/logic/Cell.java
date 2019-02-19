c2b807152f3c8ea97c8f472014af78be9b70c6c1
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-19 13:50:41 +0100

    - add primary implementation of cell class.

--- src/kasperek/logic/Cell.java
+++ src/kasperek/logic/Cell.java
@@ -1,4 +1,69 @@
 package kasperek.logic;
 
+/**
+ * @author Tomasz Kasperek
+ * @version 0.1 02/19/2019
+ * @see GameLogic
+ * @since 0.1
+ */
 public class Cell {
+    private boolean isAlive;
+    private boolean willBeAlive;
+
+    /**
+     * Default constructor with variables implementation.
+     */
+
+    public Cell() {
+        isAlive = false;
+        willBeAlive = false;
+    }
+
+    /**
+     * The method sets the cell state from alive to dead.
+     */
+
+    void killCell() {
+        willBeAlive = false;
+    }
+
+    /**
+     * The method sets the cell state from dead to alive.
+     */
+
+    void reviveCell() {
+        willBeAlive = true;
+    }
+
+    /**
+     * The method change state of cell on the negation.
+     */
+
+    void changeState() {
+        this.isAlive = !this.isAlive;
+    }
+
+    /**
+     * The method change state of cells for the next round.
+     */
+
+    void update() {
+        this.isAlive = this.willBeAlive;
+    }
+
+    /**
+     * The method return, whether the cell is alive.
+     */
+
+    boolean isAlive() {
+        return isAlive;
+    }
+
+    /**
+     * The method return, whether the cell will be alive.
+     */
+
+    boolean WillBeAlive() {
+        return willBeAlive;
+    }
 }
