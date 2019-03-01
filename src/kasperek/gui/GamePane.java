d07ddf442169e2e55fe32aa9d565adb90f55725f
Tomasz Kasperek <kasperek1993@gmail.com>
2019-03-01 20:21:12 +0100

    - add warning and statistic labels; - fix the problem with displaying error during the delete character; - add document listener for validate a value; - add validate value method; - add update method for statistics; - code refactoring;

--- src/kasperek/gui/GamePane.java
+++ src/kasperek/gui/GamePane.java
@@ -3,14 +3,15 @@ package kasperek.gui;
 import kasperek.logic.GameLogic;
 
 import javax.swing.*;
-import javax.swing.text.NumberFormatter;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 /**
  * @author Tomasz Kasperek
- * @version 0.6 02/28/2019
+ * @version 1.0 03/01/2019
  * @see GameWindow
  * @see GameLogic
  * @since 0.1
@@ -23,9 +24,15 @@ public class GamePane extends JPanel {
     private JButton allStepsButton;
     private JButton startGameButton;
 
-    private JLabel warningText;
+    private JLabel warningTextLabel;
+    private JLabel aliveCellsCounterLabel;
+    private JLabel notAliveCellsCounterLabel;
+    private JLabel willBeAliveCellsCounterLabel;
+    private JLabel willNotBeAliveCellsCounterLabel;
+    private JLabel stepsCounterLabel;
+    private JLabel initialValueOfAliveCellsLabel;
 
-    private JFormattedTextField numbersOfNeighboursTextField;
+    private JFormattedTextField numberOfAliveCellsTextField;
 
     /**
      * Default pane constructor. It is responsibility for setter all needed parameters for window.
@@ -33,80 +40,115 @@ public class GamePane extends JPanel {
 
     GamePane() {
         game = new GameLogic();
-
         this.setLayout(null);
         this.setBackground(Color.DARK_GRAY);
 
-        initializeNumbersOfNeighboursLabel();
-        initializeNumbersOfNeighboursTextField();
-        initializeWarningLabelText();
+        initialization();
+    }
+
+    private void initialization() {
+        // Text field
+        initializeNumberOfAliveCellsLabel();
+        initializeNumberOfAliveCellsTextField();
+        initializeWarningLabel();
+
+        // Start/Reset buttons
         initializeStartGameButton();
         initializeResetGameButton();
+
+        // Statistics
+        initializeStatisticsLabel();
+        initializeAliveCellsCounterLabel();
+        initializeNotAliveCellsCounterLabel();
+        initializeWillBeAliveCellsCounterLabel();
+        initializeWillNotBeAliveCellsCounterLabel();
+        initializeStepsCounterLabel();
+        initializeInitialValueOfAliveCellsLabel();
+
+        // Next/All step buttons
         initializeNextStepButton();
         initializeAllStepButton();
+
+        // Legend
         initializeLegendLabel();
+        initializeAliveTextLabel();
+        initializeNotAliveTextLabel();
     }
 
-    private void initializeNumbersOfNeighboursLabel() {
-        JLabel numbersOfNeighboursLabel = new JLabel("Numbers of neighbours:");
-        numbersOfNeighboursLabel.setBounds(983, 25, 155, 20);
-        numbersOfNeighboursLabel.setForeground(Color.LIGHT_GRAY);
+    private void initializeNumberOfAliveCellsLabel() {
+        JLabel numberOfAliveCellsLabel = new JLabel("Number of alive cells:");
+        numberOfAliveCellsLabel.setBounds(983, 25, 155, 20);
+        numberOfAliveCellsLabel.setForeground(Color.LIGHT_GRAY);
 
-        this.add(numbersOfNeighboursLabel);
-        numbersOfNeighboursLabel.setVisible(true);
+        this.add(numberOfAliveCellsLabel);
+        numberOfAliveCellsLabel.setVisible(true);
     }
 
-    private void initializeNumbersOfNeighboursTextField() {
-        NumberFormatter nf = new NumberFormatter();
-        nf.setMinimum(20);
-        nf.setMaximum(1000);
+    private void initializeNumberOfAliveCellsTextField() {
+        numberOfAliveCellsTextField = new JFormattedTextField();
+        numberOfAliveCellsTextField.setBounds(980, 42, 200, 30);
+        numberOfAliveCellsTextField.setHorizontalAlignment(JTextField.CENTER);
+        numberOfAliveCellsTextField.setBackground(Color.GRAY);
+        numberOfAliveCellsTextField.setForeground(Color.LIGHT_GRAY);
 
-        numbersOfNeighboursTextField = new JFormattedTextField(nf);
-        numbersOfNeighboursTextField.setBounds(980, 42, 200, 30);
-        numbersOfNeighboursTextField.setHorizontalAlignment(JTextField.CENTER);
-        numbersOfNeighboursTextField.setBackground(Color.GRAY);
-        numbersOfNeighboursTextField.setForeground(Color.LIGHT_GRAY);
-
-        numbersOfNeighboursTextField.addKeyListener(new KeyAdapter() {
+        numberOfAliveCellsTextField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 char input = e.getKeyChar();
 
-                if (input < '0' || input > '9') {
+                if ((input < '0' || input > '9') && input != '\b') {
                     e.consume();
-                    warningText.setText("You can type only numbers!");
-                    warningText.setVisible(true);
+                    warningTextLabel.setText("You can type only numbers!");
+                    warningTextLabel.setVisible(true);
                 } else {
-                    warningText.setVisible(false);
+                    warningTextLabel.setVisible(false);
                 }
             }
         });
 
-        this.add(numbersOfNeighboursTextField);
-        numbersOfNeighboursTextField.setVisible(true);
+        numberOfAliveCellsTextField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                validateDocumentListener(numberOfAliveCellsTextField.getText());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                validateDocumentListener(numberOfAliveCellsTextField.getText());
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                validateDocumentListener(numberOfAliveCellsTextField.getText());
+            }
+        });
+
+        this.add(numberOfAliveCellsTextField);
+        numberOfAliveCellsTextField.setVisible(true);
     }
 
-    private void initializeWarningLabelText() {
-        warningText = new JLabel();
-        warningText.setBounds(983, 67, 200, 20);
-        warningText.setForeground(Color.RED);
+    private void initializeWarningLabel() {
+        warningTextLabel = new JLabel();
+        warningTextLabel.setBounds(983, 67, 200, 20);
+        warningTextLabel.setForeground(Color.RED);
 
-        this.add(warningText);
-        warningText.setVisible(false);
+        this.add(warningTextLabel);
+        warningTextLabel.setVisible(false);
     }
 
     private void initializeStartGameButton() {
         startGameButton = new JButton("Start game");
-        startGameButton.setBounds(980, 100, 100, 40);
+        startGameButton.setBounds(980, 90, 100, 40);
 
         startGameButton.addActionListener(e -> {
-            game.startGame(Integer.valueOf(numbersOfNeighboursTextField.getText()));
-            startGameButton.setEnabled(false);
-            numbersOfNeighboursTextField.setEditable(false);
-            nextStepButton.setEnabled(true);
-            allStepsButton.setEnabled(true);
-            this.repaint();
+            String numberOfNeighbours = numberOfAliveCellsTextField.getText().replaceAll("\\W", "");
+            initialValueOfAliveCellsLabel.setText("Initial value of alive cells: " + numberOfNeighbours);
+
+            game.startGame(Integer.valueOf(numberOfNeighbours));
+            setValuesAfterStart();
         });
+
+        startGameButton.setEnabled(false);
 
         this.add(startGameButton);
         startGameButton.setVisible(true);
@@ -114,28 +156,85 @@ public class GamePane extends JPanel {
 
     private void initializeResetGameButton() {
         JButton resetGameButton = new JButton("Reset game");
-        resetGameButton.setBounds(1080, 100, 100, 40);
+        resetGameButton.setBounds(1080, 90, 100, 40);
 
-        resetGameButton.addActionListener(e -> {
-            game = new GameLogic();
-            numbersOfNeighboursTextField.setText("");
-            numbersOfNeighboursTextField.setEditable(true);
-            startGameButton.setEnabled(true);
-            nextStepButton.setEnabled(false);
-            allStepsButton.setEnabled(false);
-            this.repaint();
-        });
+        resetGameButton.addActionListener(e -> setDefaultDataAfterReset());
 
         this.add(resetGameButton);
         resetGameButton.setVisible(true);
     }
 
+    private void initializeStatisticsLabel() {
+        JLabel statisticsTitleLabel = new JLabel("Statistics:");
+        statisticsTitleLabel.setBounds(965, 140, 80, 20);
+        statisticsTitleLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(statisticsTitleLabel);
+        statisticsTitleLabel.setVisible(true);
+    }
+
+    private void initializeAliveCellsCounterLabel() {
+        aliveCellsCounterLabel = new JLabel("Alive: -");
+        aliveCellsCounterLabel.setBounds(990, 164, 200, 20);
+        aliveCellsCounterLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(aliveCellsCounterLabel);
+        aliveCellsCounterLabel.setVisible(true);
+    }
+
+    private void initializeNotAliveCellsCounterLabel() {
+        notAliveCellsCounterLabel = new JLabel("Not alive: -");
+        notAliveCellsCounterLabel.setBounds(990, 194, 200, 20);
+        notAliveCellsCounterLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(notAliveCellsCounterLabel);
+        notAliveCellsCounterLabel.setVisible(true);
+    }
+
+    private void initializeWillBeAliveCellsCounterLabel() {
+        willBeAliveCellsCounterLabel = new JLabel("Will be alive: -");
+        willBeAliveCellsCounterLabel.setBounds(970, 224, 200, 20);
+        willBeAliveCellsCounterLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(willBeAliveCellsCounterLabel);
+        willBeAliveCellsCounterLabel.setVisible(true);
+    }
+
+    private void initializeWillNotBeAliveCellsCounterLabel() {
+        willNotBeAliveCellsCounterLabel = new JLabel("Won't be alive: -");
+        willNotBeAliveCellsCounterLabel.setBounds(970, 254, 200, 20);
+        willNotBeAliveCellsCounterLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(willNotBeAliveCellsCounterLabel);
+        willNotBeAliveCellsCounterLabel.setVisible(true);
+    }
+
+    private void initializeStepsCounterLabel() {
+        stepsCounterLabel = new JLabel("Steps counter: -");
+        stepsCounterLabel.setBounds(970, 284, 200, 20);
+        stepsCounterLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(stepsCounterLabel);
+        stepsCounterLabel.setVisible(true);
+    }
+
+    private void initializeInitialValueOfAliveCellsLabel() {
+        initialValueOfAliveCellsLabel = new JLabel("Initial value of alive cells: -");
+        initialValueOfAliveCellsLabel.setBounds(970, 344, 200, 20);
+        initialValueOfAliveCellsLabel.setForeground(Color.LIGHT_GRAY);
+
+        this.add(initialValueOfAliveCellsLabel);
+        initialValueOfAliveCellsLabel.setVisible(true);
+    }
+
     private void initializeNextStepButton() {
         nextStepButton = new JButton("Next step");
-        nextStepButton.setBounds(980, 550, 100, 40);
+        nextStepButton.setBounds(980, 390, 100, 40);
 
         nextStepButton.addActionListener(e -> {
             game.singleStep();
+            updateStatistic(game.getNumberOfAliveCells(), game.getNumberOfNotAliveCells(),
+                    game.getNumberOfWillBeAlive(), game.getNumberOfWillNotBeAliveCells(), game.getStepsCounter());
             this.repaint();
         });
 
@@ -147,12 +246,13 @@ public class GamePane extends JPanel {
 
     private void initializeAllStepButton() {
         allStepsButton = new JButton("All steps");
-        allStepsButton.setBounds(1080, 550, 100, 40);
+        allStepsButton.setBounds(1080, 390, 100, 40);
 
         allStepsButton.addActionListener(e -> {
             game.allSteps(this);
             this.repaint();
             nextStepButton.setEnabled(false);
+            allStepsButton.setEnabled(false);
         });
 
         allStepsButton.setEnabled(false);
@@ -163,24 +263,90 @@ public class GamePane extends JPanel {
 
     private void initializeLegendLabel() {
         JLabel legendText = new JLabel("Legend:");
-        JLabel aliveText = new JLabel("- Alive");
-        JLabel notAliveText = new JLabel("- Not alive");
-
-        legendText.setBounds(965, 665, 80, 30);
+        legendText.setBounds(965, 670, 80, 20);
         legendText.setForeground(Color.LIGHT_GRAY);
 
-        aliveText.setBounds(990, 688, 45, 30);
+        this.add(legendText);
+        legendText.setVisible(true);
+    }
+
+    private void initializeAliveTextLabel() {
+        JLabel aliveText = new JLabel("- Alive");
+        aliveText.setBounds(990, 694, 45, 20);
         aliveText.setForeground(Color.LIGHT_GRAY);
 
-        notAliveText.setBounds(990, 713, 70, 30);
+        this.add(aliveText);
+        aliveText.setVisible(true);
+    }
+
+    private void initializeNotAliveTextLabel() {
+        JLabel notAliveText = new JLabel("- Not alive");
+        notAliveText.setBounds(990, 719, 70, 20);
         notAliveText.setForeground(Color.LIGHT_GRAY);
 
-        this.add(legendText);
-        this.add(aliveText);
         this.add(notAliveText);
-        legendText.setVisible(true);
-        aliveText.setVisible(true);
         notAliveText.setVisible(true);
+    }
+
+    public void updateStatistic(int aliveCells, int notAliveCells, int willBeAliveCells, int willNotBeAlive, int steps) {
+        aliveCellsCounterLabel.setText("Alive: " + aliveCells);
+        notAliveCellsCounterLabel.setText("Not alive: " + notAliveCells);
+        willBeAliveCellsCounterLabel.setText("Will be alive: " + willBeAliveCells);
+        willNotBeAliveCellsCounterLabel.setText("Won't be alive: " + willNotBeAlive);
+        stepsCounterLabel.setText("Steps counter: " + steps);
+    }
+
+    private void setValuesAfterStart() {
+        updateStatistic(game.getNumberOfAliveCells(), game.getNumberOfNotAliveCells(),
+                game.getNumberOfWillBeAlive(), game.getNumberOfWillNotBeAliveCells(), game.getStepsCounter());
+        startGameButton.setEnabled(false);
+        numberOfAliveCellsTextField.setEditable(false);
+        nextStepButton.setEnabled(true);
+        allStepsButton.setEnabled(true);
+        this.repaint();
+    }
+
+    private void setDefaultDataAfterReset() {
+        game = new GameLogic();
+
+        numberOfAliveCellsTextField.setText("");
+        aliveCellsCounterLabel.setText("Alive: -");
+        notAliveCellsCounterLabel.setText("Not alive: -");
+        willNotBeAliveCellsCounterLabel.setText("Won't be alive: -");
+        stepsCounterLabel.setText("Steps counter: -");
+        initialValueOfAliveCellsLabel.setText("Initial value of alive cells: -");
+
+        numberOfAliveCellsTextField.setEditable(true);
+        startGameButton.setEnabled(true);
+        nextStepButton.setEnabled(false);
+        allStepsButton.setEnabled(false);
+
+        this.repaint();
+    }
+
+    private void validateDocumentListener(String validateText) {
+        int validateNumber;
+
+        if (validateText.isEmpty()) {
+            startGameButton.setEnabled(false);
+            return;
+        } else {
+            validateNumber = Integer.valueOf(validateText);
+        }
+
+        if (validateNumber < 20) {
+            warningTextLabel.setText("The value mustn't have less than 20");
+            warningTextLabel.setVisible(true);
+            startGameButton.setEnabled(false);
+        } else if (validateNumber > 1575) {
+            warningTextLabel.setText("The value mustn't have more than 1575");
+            warningTextLabel.setVisible(true);
+            startGameButton.setEnabled(false);
+        } else {
+            warningTextLabel.setText("");
+            warningTextLabel.setVisible(false);
+            startGameButton.setEnabled(true);
+        }
     }
 
     @Override
@@ -209,18 +375,30 @@ public class GamePane extends JPanel {
             g.drawLine(marginX, i * cellSize + marginY, x * cellSize + marginX, i * cellSize + marginY);
         }
 
+        // Statistics
+        g.fillRect(960, 160, 2, 205);
+        g.fillRect(1195, 160, 2, 205);
+        g.fillRect(960, 160, 235, 2);
+        g.fillRect(960, 365, 237, 2);
+
+        g.drawOval(965, 165, 20, 20);
+        g.drawOval(965, 195, 20, 20);
+
         // Legend
-        g.fillRect(960, 668, 2, 75);
-        g.fillRect(1195, 668, 2, 75);
-        g.fillRect(960, 668, 235, 2);
+        g.fillRect(960, 690, 2, 55);
+        g.fillRect(1195, 690, 2, 55);
+        g.fillRect(960, 690, 235, 2);
         g.fillRect(960, 743, 237, 2);
 
         g.drawOval(965, 695, 20, 20);
-
-        g.setColor(Color.getColor("aliveCell"));
-        g.fillOval(965, 720, 20, 20);
-        g.setColor(Color.GRAY);
         g.drawOval(965, 720, 20, 20);
+
+        // Statistics
+        g.setColor(Color.getColor("aliveCell"));
+        g.fillOval(965, 165, 20, 20);
+
+        // Legend
+        g.fillOval(965, 695, 20, 20);
 
         game.paintBoard(g);
     }
